56da901374060769be596690680fa44b129244e4
cjdufault <dufaultconnor@gmail.com>
2020-10-01 16:52:41 -0500

    add comment explaining choice of displayed timezone

--- weather_api/weather.py
+++ weather_api/weather.py
@@ -24,30 +24,35 @@ def main():
         if location == None:
             location = input('Enter location:  ')
         
-        """I'm allowing more flexible location input from the user than
-        'city, country code' because the API is able to handle somewhat
-        ambiguous place names. This allows for searching for places with
-        the same name as a much better known place, such as Minneapolis, Kansas
+        """
+        I'm allowing more flexible location input from the user than 'city,
+        country code' because the API is able to handle somewhat ambiguous
+        place names. This allows for searching for places with the same name as
+        a much better known place, such as Minneapolis, Kansas
         ('q=minneapolis,ks,us' works for this), and searching for cities whose
-        country is unknown to the user for whatever reason."""
+        country is unknown to the user for whatever reason.
+        """
             
         # use metric by default, api uses kelvin by default
         if units == None:
             units = 'metric'
             
         parameters = {'q': location, 'units': units, 'appid': WEATHER_KEY}
-        response = requests.get(API_URL, params=parameters).json()
+        data = requests.get(API_URL, params=parameters).json()
         
-        if response['cod'] == '200':
-            show_forecast(response['list'], units)
+        if data['cod'] == '200':
+            show_forecast(data['list'], 
+                          data['city']['name'] + data['city']['country'],
+                          units)
+            
         else:
-            print(response['message'])  # show message from api if code not 200
+            print(data['message'])  # show message from api if code not 200
                 
     else:
         print('No API key found. Set environment variable "WEATHER_KEY".')
         
 
-def show_forecast(forecast_list, units):
+def show_forecast(forecast_list, location, units):
     # specify what unit symbols should be used based on the unit system
     if units.lower() == 'metric':
         temp_unit = '°C'
@@ -58,8 +63,21 @@ def show_forecast(forecast_list, units):
     else:
         temp_unit = 'K'
         speed_unit = 'm/s'
+        
+    print(f'Weather for {location} (all times in this computer\'s local time):')
+    
+    """
+    I decided to display time in the user's local time. I figured it would
+    allow the user to easily figure out, say, how long it will be until it
+    starts raining in Tunisia if they can compare their clock to the listed
+    timestamp. This approach does obscure the day/night cycle in places outside
+    of the user's local timezone, though, so it's not ideal for all purposes.
+    If I were spending more time on this, I'd probably make the timezone of the
+    displayed timestamp an option that the user could choose.
+    """
     
     for forecast in forecast_list:
+        
         timestamp = forecast['dt']
         date = datetime.fromtimestamp(timestamp)
         
@@ -67,7 +85,7 @@ def show_forecast(forecast_list, units):
         desc = forecast['weather'][0]['main']
         windspeed = forecast['wind']['speed']
         
-        print(f'At {date}, {desc:6} and {temp:6.2f}{temp_unit} with {windspeed:6.2f} {speed_unit} winds')
+        print(f'At {date}, {desc:7} and {temp:6.2f}{temp_unit} with {windspeed:6.2f} {speed_unit} winds')
         
 
 # returns a list with a length equal to the number of expected arguments, with
